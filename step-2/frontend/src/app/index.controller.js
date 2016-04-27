28dbc87bd987ec20ae2093366f87785f34b47a9c
brlouk <loukil.brahim@gmail.com>
2016-04-27 18:07:07 +0200

    Update index.controller.js

--- step-2/frontend/src/app/index.controller.js
+++ step-2/frontend/src/app/index.controller.js
@@ -1,9 +1,16 @@
 (function() {
 	'use strict';
+
 	angular.module('myapp').controller('MyController', MyController);
 
 	function MyController($scope) {
-		$scope.hi="I'm here!";
+		$scope.hi = "I'm here!";
+	}
+	
+	angular.module('myapp').controller('MyController2', MyController2);
+
+	function MyController2() {
+		this.hi="I'm here!";
 	}
 
 })();
