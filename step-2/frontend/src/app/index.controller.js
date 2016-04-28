17b50ae1ca29759d54571b2b110f863722fd287d
brlouk <loukil.brahim@gmail.com>
2016-04-28 16:20:46 +0200

    Update index.controller.js

--- step-2/frontend/src/app/index.controller.js
+++ step-2/frontend/src/app/index.controller.js
@@ -4,13 +4,67 @@
 	angular.module('myapp').controller('MyController', MyController);
 
 	function MyController($scope) {
-		$scope.hi = "I'm here!";
+		$scope.hi = "Hi! My name is $scope!";
 	}
-	
-	angular.module('myapp').controller('MyController2', MyController2);
 
-	function MyController2() {
-		this.hi="I'm here!";
+	angular.module('myapp').controller('ActorController1',
+			ActorController1);
+
+	function ActorController1() {
+		var vm = this;
+		vm.name = "Bruce Wayne";
+	}
+
+	angular.module('myapp').controller('ActorController2',
+			ActorController2);
+
+	function ActorController2() {
+		var vm = this;
+		vm.displayName = "";
+
+		vm.showActorName = function() {
+			vm.displayName = "Bruce Wayne";
+		}
+	}
+
+	angular.module('myapp').controller('CustomerController',
+			CustomerController3);
+
+	function CustomerController3() {
+		var vm = this;
+
+		vm.firstname = "";
+		vm.lastname = "";
+
+		vm.submit = function() {
+			vm.newcustomer.firstname = vm.firstname;
+			vm.newcustomer.lastname = vm.lastname;
+		}
+
+		vm.reset = function() {
+			vm.newcustomer = {};
+		}
+
+		vm.reset();
+	}
+
+	angular.module('myapp').controller('ActorController3', ActorController3);
+
+	function ActorController3() {
+		var vm = this;
+		vm.list = [ {
+			"firstname" : "Bruce",
+			"lastname" : "Wayne"
+		}, {
+			"firstname" : "Jet",
+			"lastname" : "Li"
+		}, {
+			"firstname" : "liam",
+			"lastname" : "Neeson"
+		}, {
+			"firstname" : "Maggie",
+			"lastname" : "Grace"
+		} ];
 	}
 
 })();
